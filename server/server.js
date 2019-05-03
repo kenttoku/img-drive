0250effaff89107161fbf9a03ddfdaccf77add1a
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-03 11:36:24 -0500

    Update PORT and start script

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 app.use(express.json());
