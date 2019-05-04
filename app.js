c52f5bba49ed2fdf002573f829d729adc68b7694
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-04 18:07:57 -0500

    Add packages for users

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 
 const { PORT } = require('./config');
 const db = require('./db');
 const imageRouter = require('./routers/image-router');
-const User = require('./models/user-model');
-
 // Create an Express app
 const app = express();
 
@@ -23,17 +22,9 @@ app.use('/api/images', imageRouter);
 
 // Listen for incoming connections
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server up on PORT ${PORT}`);
 
-  db
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
-
-  User.create({ username: 'kent', password: 'password123' });
+  db.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database:', err));
 });
