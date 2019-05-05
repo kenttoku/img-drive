a0d0b64b2d8c68b2d6d188171bd9502d37dd8e34
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-05 15:39:25 -0500

    Create a refresh endpoint

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const passport = require('passport');
+
 const router = express.Router();
 const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 const createAuthToken = require('../utils/create-auth-token');
 
 router.post('/', localAuth, (req, res) => {
   const authToken = createAuthToken({ username: req.user.username });
+  res.json(authToken);
+});
+
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
   res.json(authToken);
 });
 
