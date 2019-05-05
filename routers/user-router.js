09b2a05c6a5e698ecfdcb33be30eedfdb7b9af13
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-05 14:25:44 -0500

    Minor changes to catch block

--- routers/user-router.js
+++ routers/user-router.js
@@ -10,13 +10,7 @@ router.post('/', (req, res, next) => {
     password
   }).then(result => {
     return res.status(201).location(`/api/users/${result.id}`).json(result);
-  }).catch(err => {
-    if (err.code === 11000) {
-      err = new Error('The username already exists');
-      err.status = 400;
-    }
-    next(err);
-  });
+  }).catch(err => next(err));
 });
 
 module.exports = router;
